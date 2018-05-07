000dfe43de0042edec5a96f7a5ba386fe502e089
Simon Bihel <simon.bihel@ens-rennes.fr>
2018-05-07 16:59:56 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pr_message_gen',
+      version='0.1',
+      description='PR message generator for DSpot amplified test cases.',
+      url='https://github.com/sbihel/internship_amplification',
+      author='Simon Bihel',
+      author_email='simon.bihel@ens-rennes.fr',
+      license='MIT',
+      packages=['pr_message_gen'],
+      install_requires=[
+          'gitpython',
+      ],
+      zip_safe=False)
