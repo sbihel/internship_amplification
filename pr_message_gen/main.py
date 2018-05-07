b405c11c67d40d69a9a54b27ca45132169a701b6
Simon Bihel <simon.bihel@ens-rennes.fr>
2018-05-07 13:47:35 +0200

    Use dspot.properties instead of silly arguments

--- pr_message_gen/main.py
+++ pr_message_gen/main.py
@@ -4,7 +4,7 @@
 """
 import argparse
 import json
-from os import path
+from os import path, listdir, sep
 from collections import Counter
 
 
@@ -24,6 +24,8 @@ def describe_amplification(amplification):
     elif categ == "MODIFY":
         return "Modified " + amplification["role"] + ", from " + \
             amplification["oldValue"] + " to " + amplification["newValue"]
+    else:
+        raise ValueError("Unknown category.")
 
 
 def describe_asserts(new_asserts):
@@ -35,8 +37,8 @@ def describe_asserts(new_asserts):
     count_asserts = Counter(new_asserts_shortname)
     res = ""
     for shortname in count_asserts:
-        res += str(count_asserts[shortname]
-                   ) + " assertions for" + shortname + "\n"
+        res += str(count_asserts[shortname]) + \
+            " assertions for the return value of `" + shortname + "`\n"
     return res[:-1]
 
 
@@ -49,41 +51,19 @@ def describe_mutant(mutant):
         str(mutant["lineNumber"]) + ", using " + mutant["ID"].split('.')[-1]
 
 
-def main():
+def describe_test_class(test_class_report_path):
     """
-    Function to run pr_message_gen.
+    Natural language description of an amplified test class.
     """
-    command_parser = argparse.ArgumentParser()
-    command_parser.add_argument(
-        '-amplog', '--amplification_log', type=str,
-        help='json file containing the amplification log')
-    command_parser.add_argument(
-        '-mutants', '--mutation_score', type=str,
-        help='json file containing the mutation score report')
-    command_parser.add_argument(
-        '-test', '--test', type=str,
-        help='path for both mutation_score report and amplification log')
-
-    opts, _ = command_parser.parse_known_args()
-
-    if opts.test:
-        if not path.exists(opts.test + '_mutants_killed.json'):
-            return
-        print("========== Class: " + opts.test.split('.')[-1] + " ==========")
-        with open(opts.test+'_mutants_killed.json', 'r') as json_file:
-            mutation_score = json.load(json_file)
-        with open(opts.test+'_amp_log.json', 'r') as json_file:
-            amplification_log = json.load(json_file)
-    else:
-        if not path.exists(opts.mutation_score):
-            return
-        print(
-            "========== Class: " + opts.mutation_score.split('.')[-2] +
-            " ==========")
-        with open(opts.mutation_score, 'r') as json_file:
-            mutation_score = json.load(json_file)
-        with open(opts.amplification_log, 'r') as json_file:
-            amplification_log = json.load(json_file)
+    if not path.exists(test_class_report_path + '_mutants_killed.json'):
+        return
+    print(
+        "========== Class: " + test_class_report_path.split('.')[-1] +
+        " ==========")
+    with open(test_class_report_path+'_mutants_killed.json', 'r') as json_file:
+        mutation_score = json.load(json_file)
+    with open(test_class_report_path+'_amp_log.json', 'r') as json_file:
+        amplification_log = json.load(json_file)
 
     amplified_tests = (test_case["name"]
                        for test_case in mutation_score["testCases"])
@@ -94,7 +74,8 @@ def main():
         else:
             if i:
                 print()
-            print('===== Generated test: ' + amplified_test + ' =====')
+            print('===== Generated test: `' + amplified_test + '` from `' +
+                  mutation_score["testCases"][i]["parentName"] + '` =====')
             new_asserts = [
                 amplification
                 for amplification in amplification_log[amplified_test]
@@ -109,6 +90,59 @@ def main():
             for mutant in mutation_score["testCases"][i]["mutantsKilled"]:
                 print(describe_mutant(mutant))
         i += 1
+
+
+def main():
+    """
+    Function to run pr_message_gen.
+    """
+    command_parser = argparse.ArgumentParser()
+    command_parser.add_argument('-p', '--properties_file', type=str,
+                                help='dspot.properties file.')
+    command_parser.add_argument('-t', '--test', type=str,
+                                help='Name of a particular test class.')
+
+    opts, _ = command_parser.parse_known_args()
+
+    if not opts.properties_file:
+        raise ValueError("Need properties file")
+
+    dir_prop = path.join(*(opts.properties_file.split(sep)[:-1]))
+    project_root = None
+    module_path = None
+    src_path = None
+    test_path = None
+    output_directory = None
+    with open(opts.properties_file, 'r') as properties_file:
+        for line in properties_file:
+            line = line.strip()
+            if line.startswith('project='):
+                project_root = line[len("project="):]
+            elif line.startswith('targetModule='):
+                module_path = line[len('targetModule='):]
+            elif line.startswith('src='):
+                src_path = line[len('src='):]
+            elif line.startswith('testSrc='):
+                test_path = line[len('testSrc='):]
+            elif line.startswith('outputDirectory='):
+                output_directory = line[len('outputDirectory='):]
+
+    if opts.test:
+        test_class_report_path = path.join(dir_prop,
+                                           project_root,
+                                           module_path,
+                                           output_directory,
+                                           opts.test)
+        if not path.exists(test_class_report_path+"_mutants_killed.json"):
+            raise ValueError("Test class report not found.")
+        describe_test_class(test_class_report_path)
+    else:  # describe every amplified test class
+        report_dir = path.join(dir_prop, project_root,
+                                module_path, output_directory)
+        for file in listdir(report_dir):
+            if file.endswith('_mutants_killed.json'):
+                describe_test_class(
+                    report_dir + sep + file[: -len('_mutants_killed.json')])
 
 
 if __name__ == '__main__':
