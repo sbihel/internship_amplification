acb009a35ea4bd1187742048c56e75e3d17d31dd
Simon Bihel <simon.bihel@ens-rennes.fr>
2018-05-21 14:51:42 +0200

    Add PIT mutants description

--- pr_message_gen/main.py
+++ pr_message_gen/main.py
@@ -140,10 +140,18 @@ def describe_mutants(mutants, project_root_path, module_path, src_path):
         link = git_link.create_url_file_line(
             project_root_path, os.path.join(
                 module_path, src_path, class_path), line_number)
-        res += "The new test can detect " + str(nb_mutants) + " change" + \
-            ('s' if nb_mutants > 1 else '') + " in `" + \
+        res += "#### The new test can detect " + str(nb_mutants) + \
+            " change" + ('s' if nb_mutants > 1 else '') + " in `" + \
             class_mutated.split('.')[-1] + '#' + method_mutated + \
             "`, line " + str(line_number) + ". " + link + "\n"
+        i = 0
+        for full_mutant in mutants:
+            if (full_mutant['lineNumber'] == mutant[0] and
+                    full_mutant['locationClass'] == mutant[1] and
+                    full_mutant['locationMethod'] == mutant[2]):
+                i += 1
+                res += str(i) + '. ' + full_mutant['description'] + '\n'
+        res += '\n'
     return res[:-1]
 
 
