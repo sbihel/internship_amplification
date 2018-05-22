6faaf200fd6397e5aca71b190d4c958d51fd9da7
Simon Bihel <simon.bihel@ens-rennes.fr>
2018-05-22 14:07:49 +0200

    Fix assign amp

--- pr_message_gen/asserts.py
+++ pr_message_gen/asserts.py
@@ -22,6 +22,62 @@ def __is_assert(assert_stmt):
     return (not __is_trycatch(assert_stmt)) and (not __is_assign(assert_stmt))
 
 
+def __get_trycatch_target(trycatch_stmt):
+    return trycatch_stmt.split('} catch (')[-1].split(' ')[0]
+
+
+def __get_assign_target(assign_stmt):
+    return '='.join(assign_stmt.split('=')[1:]).strip()
+
+
+def __get_assert_target(assert_stmt):
+    # parse the line, extracting the target of the assert
+    # javalang was taking a long time, and could not pretty print
+    position = 0  # position of the arguments of the assert
+    for char in assert_stmt:
+        position += 1
+        if char == '(':
+            break
+    position_1st = position
+
+    looking_for = [',']
+    skip_next = False
+    for char in assert_stmt[position_1st:]:
+        position += 1
+        if skip_next:
+            skip_next = False
+            continue
+
+        if char == looking_for[0]:
+            looking_for.pop(0)
+            if looking_for == []:
+                break
+        elif char == '\"':
+            looking_for = ['\"'] + looking_for
+        elif char == '(':
+            looking_for = [')'] + looking_for
+        elif char == '\\':
+            skip_next = True
+            continue
+
+    if looking_for != []:  # only 1 argument, go back to beginning
+        position = position_1st
+    return assert_stmt[position:-1].strip()
+
+
+def __get_a_amp_target(a_amp):
+    """
+    Extract the variable/method tested by an a-amplification.
+    """
+    assert_stmt = a_amp["newValue"]
+    if __is_trycatch(assert_stmt):
+        return __get_trycatch_target(assert_stmt)
+    if __is_assign(assert_stmt):
+        return __get_assign_target(assert_stmt)
+    # assert statement
+    return __get_assert_target(assert_stmt)
+
+
 def __sort_asserts(a_amps):
     trycatchs = []
     assigns = []
@@ -37,57 +93,13 @@ def __sort_asserts(a_amps):
     return trycatchs, assigns, asserts
 
 
-def __get_assert_target(assertion):
-    """
-    Extract the variable/method tested by an assertion.
-    """
-    assert_stmt = assertion["newValue"]
-    if __is_trycatch(assert_stmt):
-        return assert_stmt.split('} catch (')[-1].split(' ')[0]
-    elif __is_assign(assert_stmt):
-        return assert_stmt.split('=')[-1].lstrip()
-    else:  # assert statement
-        # parse the line, extracting the target of the assert
-        # javalang was taking a long time, and could not pretty print
-        position = 0  # position of the arguments of the assert
-        for char in assert_stmt:
-            position += 1
-            if char == '(':
-                break
-        position_1st = position
-
-        looking_for = [',']
-        skip_next = False
-        for char in assert_stmt[position_1st:]:
-            position += 1
-            if skip_next:
-                skip_next = False
-                continue
-
-            if char == looking_for[0]:
-                looking_for.pop(0)
-                if looking_for == []:
-                    break
-            elif char == '\"':
-                looking_for = ['\"'] + looking_for
-            elif char == '(':
-                looking_for = [')'] + looking_for
-            elif char == '\\':
-                skip_next = True
-                continue
-
-        if looking_for != []:  # only 1 argument, go back to beginning
-            position = position_1st
-        return assert_stmt[position:-1].strip()
-
-
 def __get_variable_name(assign):
     return assign['newValue'].split(' ')[1]
 
 
 def __describe_trycatch(trycatch):
     res = "#### Generated an exception handler for " + \
-        __get_assert_target(trycatch) + ".\n\n"
+        __get_a_amp_target(trycatch) + ".\n\n"
     lines = trycatch["newValue"].split('\n')
     lines[0] = '+ ' + lines[0]
     lines[-1] = '+ ' + lines[-1]
@@ -100,6 +112,41 @@ def __describe_trycatch(trycatch):
     for i in range(1, len(lines)-nb_lines_modified_end):
         lines[i] = '  ' + lines[i]
     res += "```diff\n" + '\n'.join(lines) + "\n```\n"
+    return res
+
+
+def __describe_assign(assign, new_asserts, new_asserts_targets, nb_asserts):
+    res = ""
+    assign_variable = __get_variable_name(assign)
+    i = 0
+    related_asserts = []
+    asserts_done = []
+    for assertion in new_asserts:
+        if assign_variable in new_asserts_targets[i]:
+            related_asserts += [assertion]
+            asserts_done += [i]
+        i += 1
+    res += "#### Generated " + str(len(related_asserts)) + " assertion" + \
+        ('s' if len(related_asserts) > 1 else '') + \
+        " for the observations from `" + __get_a_amp_target(assign) + \
+        "`.\n\n"
+    res += "```diff\n+ " + assign['newValue'].replace('\n', '\n+ ') + \
+        "\n```\n\n"
+    if nb_asserts < MAX_NB_ASSERTS:
+        for related_assert in related_asserts:
+            res += "```diff\n+ " + related_assert['newValue'] + "\n```\n"
+
+    for index in reversed(asserts_done):
+        new_asserts.pop(index)
+        new_asserts_targets.pop(index)
+
+    return res
+
+
+def __describe_assign_with_assert(assign):
+    res = "#### Generated a bundle input amplification with assertions.\n"
+    res += "```diff\n+ " + assign['newValue'].replace('\n', '\n+ ') + \
+        "\n```\n\n"
     return res
 
 
@@ -116,31 +163,14 @@ def describe_asserts(a_amps):
 
     new_asserts_targets = []
     for assertion in new_asserts:
-        new_asserts_targets += [__get_assert_target(assertion)]
+        new_asserts_targets += [__get_a_amp_target(assertion)]
 
     for assign in assigns:
-        assign_variable = __get_variable_name(assign)
-        i = 0
-        related_asserts = []
-        asserts_done = []
-        for assertion in new_asserts:
-            if assign_variable in new_asserts_targets[i]:
-                related_asserts += [assertion]
-                asserts_done += [i]
-            i += 1
-        res += "#### Generated " + str(len(related_asserts)) + " assertion" + \
-            ('s' if len(related_asserts) > 1 else '') + \
-            " for the observations from `" + __get_assert_target(assign) + \
-            "`.\n\n"
-        res += "```diff\n+ " + assign['newValue'].replace('\n', '\n+ ') + \
-            "\n```\n\n"
-        if nb_asserts < MAX_NB_ASSERTS:
-            for related_assert in related_asserts:
-                res += "```diff\n+ " + related_assert['newValue'] + "\n```\n"
-
-        for index in reversed(asserts_done):
-            new_asserts.pop(index)
-            new_asserts_targets.pop(index)
+        if 'org.junit.Assert' not in assign['newValue']:  # normal assign
+            res += __describe_assign(assign, new_asserts,
+                                     new_asserts_targets, nb_asserts)
+        else:  # assign with assert
+            res += __describe_assign_with_assert(assign)
 
     count_asserts = Counter(new_asserts_targets)
     for target in count_asserts:
@@ -151,7 +181,7 @@ def describe_asserts(a_amps):
         if nb_asserts < MAX_NB_ASSERTS:  # add diff
             for assertion in new_asserts:
                 new_value = assertion["newValue"]
-                if __get_assert_target(assertion) == target:
+                if __get_a_amp_target(assertion) == target:
                     res += "```diff\n+ " + new_value + "\n```\n\n"
 
     return res[:-1]
